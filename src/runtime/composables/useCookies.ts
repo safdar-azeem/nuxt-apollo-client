82af9a2540b25770f460c246eeab55b99af65177
safdar-azeem <safdarazeem00@gmail.com>
2024-07-17 22:29:02 +0500

    Add useCookies.ts

---
+++ src/runtime/composables/useCookies.ts
@@ -0,0 +1,18 @@
+import { useNuxtApp } from '#app'
+import JSCookies from 'js-cookie'
+
+export const setToken = ({ key = '', token, expires = 1 / 720 }) => {
+  const tokenKey = key || (useNuxtApp()['tokenKey'] as string)
+
+  JSCookies.set(tokenKey, token, { expires: expires })
+}
+
+export const getToken = (key = '') => {
+  const tokenKey = key || (useNuxtApp()['tokenKey'] as string)
+  return JSCookies.get(tokenKey)
+}
+
+export const removeToken = (key = '') => {
+  const tokenKey = key || (useNuxtApp()['tokenKey'] as string)
+  JSCookies.remove(tokenKey)
+}
