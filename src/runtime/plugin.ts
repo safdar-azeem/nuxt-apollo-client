1e2d1d0a5b419f3e3199de43c6f622c8165bc48c
safdar-azeem <safdarazeem00@gmail.com>
2024-07-17 22:29:02 +0500

    Add plugin.ts

---
+++ src/runtime/plugin.ts
@@ -0,0 +1,36 @@
+import { NuxtApollo } from '#apollo'
+import { defineNuxtPlugin } from '#app'
+import { graphqlConfig } from './graphql.config'
+import { ApolloClients } from '@vue/apollo-composable'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const clients = graphqlConfig({
+    endPoints: NuxtApollo.clients || {},
+    tokenKey: NuxtApollo.tokenKey || 'token',
+    setContext: NuxtApollo?.setContext,
+  })
+
+  nuxtApp.vueApp.provide(ApolloClients, clients)
+
+  if (process.server) {
+    nuxtApp.payload.apollo = {}
+    nuxtApp.hook('app:rendered', () => {
+      for (const [key, client] of Object.entries(clients)) {
+        nuxtApp.payload.apollo[key] = client.extract()
+      }
+    })
+  }
+
+  if (process.client) {
+    const apolloState = nuxtApp.payload.apollo
+    for (const [key, state] of Object.entries(apolloState)) {
+      clients[key].restore(state)
+    }
+  }
+
+  return {
+    provide: {
+      apolloClients: clients,
+    },
+  }
+})
