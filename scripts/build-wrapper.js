f87f35fe0ad6faeb7a893fa5f34eb8f7a8981767
safdar-azeem <safdarazeem00@gmail.com>
2024-07-19 02:13:54 +0500

    Update build-wrapper.js

--- scripts/build-wrapper.js
+++ scripts/build-wrapper.js
@@ -9,13 +9,6 @@ const __dirname = path.dirname(__filename)
 
 const sourceFilePath = path.resolve(__dirname, '../src/runtime/codegen.ts')
 const distFilePath = path.resolve(__dirname, '../dist/runtime/codegen.js')
-const graphqlFilePath = path.resolve(__dirname, '../dist/runtime/composables/graphql.js')
-const graphqlDtsFilePath = path.resolve(__dirname, '../dist/runtime/composables/graphql.d.ts')
-const composablesIndexFilePath = path.resolve(__dirname, '../dist/runtime/composables/index.js')
-const composablesIndexDtsFilePath = path.resolve(
-  __dirname,
-  '../dist/runtime/composables/index.d.ts'
-)
 
 const readFile = (filePath) => fs.readFileSync(filePath, 'utf8')
 
@@ -59,34 +52,6 @@ const main = () => {
 
   writeFile(sourceFilePath, originalContent)
   console.log('Restored original content of src/runtime/codegen.ts')
-
-  fs.unlinkSync(graphqlFilePath)
-  fs.unlinkSync(graphqlDtsFilePath)
-  fs.unlinkSync(composablesIndexFilePath)
-  fs.unlinkSync(composablesIndexDtsFilePath)
-
-  fs.writeFileSync(graphqlFilePath.replace('.js', '.ts'), `export const useAnyQuery = () => {}`)
-  fs.writeFileSync(
-    graphqlDtsFilePath,
-    `
-    export const useAnyQuery: () => any;
-  `
-  )
-
-  fs.writeFileSync(
-    composablesIndexFilePath.replace('.js', '.ts'),
-    `
-  export * from "./useCookies.js";
-  export * from "./graphql";
-  `
-  )
-  fs.writeFileSync(
-    composablesIndexDtsFilePath,
-    `
-  export * from './useCookies.js';
-  export * from './graphql';
-  `
-  )
 
   console.log('Build process completed successfully!')
 }
