1ec0e5357179b08b7f74011d84fc76235d9c3475
safdar-azeem <safdarazeem00@gmail.com>
2024-07-25 15:34:32 +0500

    Update useCookies.ts

--- src/runtime/composables/useCookies.ts
+++ src/runtime/composables/useCookies.ts
@@ -1,18 +1,43 @@
 import JSCookies from 'js-cookie'
 import { NuxtApollo } from '#apollo'
 
+interface CookieAttributes {
+  expires?: number | Date | undefined
+  path?: string | undefined
+  domain?: string | undefined
+  secure?: boolean | undefined
+  sameSite?: 'strict' | 'Strict' | 'lax' | 'Lax' | 'none' | 'None' | undefined
+  [property: string]: any
+}
+
+type TokenParams = {
+  key?: string
+  token: string
+  options?: CookieAttributes
+}
+
 const getKey = (key: string) => {
   return key || NuxtApollo?.tokenKey || 'token'
 }
 
-export const setToken = ({ key = '', token, expires = 1 }) => {
-  JSCookies.set(getKey(key), token, { expires: expires })
+export const setToken = (...args: [TokenParams] | [string, string?, CookieAttributes?]): void => {
+  const {
+    key = '',
+    token,
+    options = {},
+  } = typeof args[0] === 'object'
+    ? args[0]
+    : { token: args[0], key: args[1] || '', options: args[2] || {} }
+
+  const finalOptions: CookieAttributes = { expires: 1, ...options }
+
+  JSCookies.set(getKey(key), token, finalOptions)
 }
 
 export const getToken = (key = '') => {
   return JSCookies.get(getKey(key))
 }
 
-export const removeToken = (key = '') => {
-  JSCookies.remove(getKey(key))
+export const removeToken = (key = '', options?: CookieAttributes) => {
+  JSCookies.remove(getKey(key), options)
 }
