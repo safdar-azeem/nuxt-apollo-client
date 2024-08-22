74ae58b9dac000b7604e8177c8f9bf6300a63042
safdar-azeem <safdarazeem00@gmail.com>
2024-08-22 15:55:54 +0500

    Update nuxt.config.ts

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,11 +1,14 @@
 export default defineNuxtConfig({
   modules: ['../src/module'],
   ssr: true,
+
   apollo: {
     endPoints: {
       default: 'http://localhost:4041/graphql',
     },
     prefix: 'I',
   },
+
   devtools: { enabled: true },
+  compatibilityDate: '2024-08-22',
 })
