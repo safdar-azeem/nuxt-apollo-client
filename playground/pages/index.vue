9ce90cf414e04f887c8d8796d15e1bc0254f82c7
safdar-azeem <safdarazeem00@gmail.com>
2025-03-18 04:44:31 +0500

    Add index.vue

---
+++ playground/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import UserProfile from '../component/UserProfile.vue'
+</script>
+
+<template>
+  <div>
+    <NuxtLink to="/about">About</NuxtLink>
+
+    <UserProfile />
+  </div>
+</template>
