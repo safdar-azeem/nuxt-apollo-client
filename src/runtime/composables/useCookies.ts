d18b53c6899ef1abe2c0f39abf61d0fd02bff95f
safdar-azeem <safdarazeem00@gmail.com>
2025-05-20 22:06:20 +0500

    Update useCookies.ts

--- src/runtime/composables/useCookies.ts
+++ src/runtime/composables/useCookies.ts
@@ -1,6 +1,7 @@
 import JSCookies from 'js-cookie'
 import { NuxtApollo } from '#apollo'
 import { useNuxtApp } from '#app'
+import { onMounted, onUnmounted } from 'vue'
 
 interface CookieAttributes {
   expires?: number | Date | undefined
@@ -31,7 +32,12 @@ export const setToken = (...args: [TokenParams] | [string, string?, CookieAttrib
     ? args[0]
     : { token: args[0], key: args[1] || '', options: args[2] || {} }
 
-  const finalOptions: CookieAttributes = { expires: 1, ...options }
+  const EXPIRATION_MINUTES =
+    options?.expires ||
+    NuxtApollo?.tokenExpiration ||
+    new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
+
+  const finalOptions: CookieAttributes = { ...options, expires: EXPIRATION_MINUTES }
 
   JSCookies.set(getKey(key), token, finalOptions)
 }
@@ -42,4 +48,52 @@ export const getToken = (key = '') => {
 
 export const removeToken = (key = '', options?: CookieAttributes) => {
   JSCookies.remove(getKey(key), options)
+}
+
+const ACTIVITY_EVENTS = ['click', 'mousemove', 'keydown', 'scroll']
+
+export const useKeepCookieAlive = (debounceInterval = 10000) => {
+  const DEBOUNCE_INTERVAL_MS = debounceInterval
+
+  let lastRun = 0
+  let timeout = null
+
+  const refreshToken = () => {
+    const now = Date.now()
+    const elapsed = now - lastRun
+
+    if (elapsed >= DEBOUNCE_INTERVAL_MS) {
+      const token = getToken()
+      if (token) {
+        setToken({ token })
+        lastRun = now
+      }
+    } else if (!timeout) {
+      timeout = setTimeout(() => {
+        const token = getToken()
+        if (token) {
+          setToken({ token })
+          lastRun = Date.now()
+        }
+        timeout = null
+      }, DEBOUNCE_INTERVAL_MS - elapsed)
+    }
+  }
+
+  const registerListeners = () => {
+    ACTIVITY_EVENTS.forEach((event) =>
+      window.addEventListener(event, refreshToken, { passive: true })
+    )
+  }
+
+  const removeListeners = () => {
+    ACTIVITY_EVENTS.forEach((event) => window.removeEventListener(event, refreshToken))
+    if (timeout) {
+      clearTimeout(timeout)
+      timeout = null
+    }
+  }
+
+  onMounted(registerListeners)
+  onUnmounted(removeListeners)
 }
