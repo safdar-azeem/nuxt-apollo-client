ebb921e6e4fa1574fe9210e5a5f79350fef3fc11
safdar-azeem <safdarazeem00@gmail.com>
2025-05-20 22:06:20 +0500

    Update replaceConfig.ts

--- src/runtime/utils/replaceConfig.ts
+++ src/runtime/utils/replaceConfig.ts
@@ -11,7 +11,6 @@ const replaceStrings = [
   { from: 'VueApolloComposable.UseSubscriptionReturn', to: 'UseSubscriptionReturn' },
   { from: 'VueApolloComposable.UseResultReturn', to: 'UseResultReturn' },
   { from: 'VueApolloComposable.UseApolloClientReturn', to: 'UseApolloClientReturn' },
-
   { from: 'VueApolloComposable.useQuery', to: 'useQuery' },
   { from: 'VueApolloComposable.useMutation', to: 'useMutation' },
   { from: 'VueApolloComposable.useLazyQuery', to: 'useLazyQuery' },
@@ -25,38 +24,37 @@ const replaceStrings = [
   { from: 'VueApolloComposable.provideApolloClient', to: 'provideApolloClient' },
   { from: 'VueApolloComposable.provideApolloClients', to: 'provideApolloClients' },
   { from: 'VueApolloComposable.useResult', to: 'useResult' },
-
   { from: 'VueApolloComposable.DefaultApolloClient', to: 'DefaultApolloClient' },
   { from: 'VueApolloComposable.ApolloClients', to: 'ApolloClients' },
-
-  { from: /import type\s*\*\s*as\s*VueCompositionApi\s*from\s*'vue';\s*\n?/g, to: '' },
-  { from: /export type Maybe<T> = T \| null\s*\n?/g, to: '' },
-  { from: /export type InputMaybe<T> = Maybe<T>\s*\n?/g, to: '' },
   {
-    from: /export type Exact<T extends { \[key: string\]: unknown }> = { \[K in keyof T\]: T\[K\] }\s*\n?/g,
-    to: '',
+    from: /import type\s*\*\s*as\s*VueCompositionApi\s*from\s*'vue';\s*\n?/g,
+    to: "import type * as VueCompositionApi from 'vue';\n",
   },
   { from: /^\s*;\s*\n/gm, to: '' },
+  { from: /^export type Maybe<T>\s*=.*$/gm, to: 'export type Maybe<T> = T;' },
+  {
+    from: /^export type InputMaybe<T>\s*=.*$/gm,
+    to: 'export type InputMaybe<T> = Maybe<T> | Ref<T>|ComputedRef<T>;',
+  },
+  {
+    from: /^export type Exact<T extends { \[key: string\]: unknown }>\s*=.*$/gm,
+    to: 'export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] | Ref<T[K]> | ComputedRef<T[K]>; };',
+  },
 ]
 
 const applyReplacements = (content) => {
   let updatedContent = content
-
   replaceStrings.forEach(({ from, to }) => {
-    // Handle regex patterns
     if (from instanceof RegExp) {
       updatedContent = updatedContent.replace(from, to)
     } else {
-      // Handle string replacements with word boundaries
       const regex = new RegExp(`\\b${escapeRegExp(from)}\\b`, 'g')
       updatedContent = updatedContent.replace(regex, to)
     }
   })
-
   return updatedContent
 }
 
-// Utility function to escape special regex characters
 const escapeRegExp = (string) => {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
 }
