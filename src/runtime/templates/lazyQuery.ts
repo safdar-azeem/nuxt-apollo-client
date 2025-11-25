b3aee355520243b6b3216d720df9847091e27db3
safdar-azeem <safdarazeem00@gmail.com>
2025-11-25 18:27:07 +0500

    Add lazyQuery.ts

---
+++ src/runtime/templates/lazyQuery.ts
@@ -0,0 +1,40 @@
+export const getLazyQuery = () => `
+////////////////////////////////////////////////////////////////////////////////////////
+
+const useLazyQuery = <TResult = any, TVariables = any>(
+  document,
+  variables,
+  options
+): UseLazyQueryReturn<TResult, TVariables> => {
+  const lazyQuery = apolloUseLazyQuery(document, variables, {
+    fetchPolicy: 'cache-and-network',
+    ...options,
+  }) as any
+
+  // Always keep loading as false after initial fetch
+  const loadingState = ref(false)
+   
+  async function fetchOrRefetch(newVariables?: TVariables) {
+      loadingState.value = lazyQuery.result?.value ? false : true
+     
+    try {      
+      if (!lazyQuery.result?.value) {
+        await lazyQuery?.load(document)
+        return lazyQuery.refetch(newVariables || variables)
+      } else { 
+        return lazyQuery.refetch(newVariables || variables)
+      }
+    } finally {
+      loadingState.value = false
+    }
+  }
+
+  return {
+    ...lazyQuery,
+    start: fetchOrRefetch,
+    load: fetchOrRefetch,
+    refetch: fetchOrRefetch,
+    loading: loadingState,
+  }
+}
+`
